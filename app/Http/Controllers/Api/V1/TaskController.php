ceec4a9e4c6cc4ecb8afd09fd22305235ef52dac
Sitraka <admin@MacBook-Pro-de-Sitraka.local>
2026-03-03 21:16:53 +0100

    create resource Task

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 
 class TaskController extends Controller
@@ -10,7 +11,14 @@ class TaskController extends Controller
     //
     public function index()
     {
-        return Task::all();
+        //return TaskResource::collection(Task::all());
+        return Task::all()->toResourceCollection();
+
+    }
+    public function show(Task $task){
+        //return TaskResource::make($task);
+        //return new TaskResource($task);
+        return $task->toResource();
 
     }
 }
